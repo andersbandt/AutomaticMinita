1183592c1bccf59727fcfc02dea8d43d7753e2db
andersbandt <andersbandt@yahoo.com>
2018-05-04 10:01:10 -0500

    Update relay-control.php

--- relay-control.php
+++ relay-control.php
@@ -10,14 +10,18 @@
                  <input type="submit" value="OFF" name="off">
                  <input type="text" name="hour">
                  <input type="text" name="minute">
-                 <input type="submit" value="OFF" name="final">
+                 <input type="submit" value="OFF" name="submit">
          </form>
          <?php
          $setmode17 = shell_exec("/usr/local/bin/gpio -g mode 17 out");
          if(isset($_GET['final'])){
                  $hour = $_GET['hour'];
                  $minute = $_GET['minute']; 
-          
+                 $output = shell_exec('crontab -l');
+                 $command = $minute + $hour + '* * * STUFF TO DO!!!!
+                 file_put_contents('/tmp/crontab.txt', $output.' * * * * NEW_CRON'.PHP_EOL);
+                 echo exec('crontab /tmp/crontab.txt');
+         }
          if(isset($_GET['on'])){
                  $gpio_on = shell_exec("/usr/local/bin/gpio -g write 17 1");
                 echo "LED is on";
