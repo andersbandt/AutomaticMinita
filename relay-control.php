51f0c1a679e5710befa9525915333cf2ca014f74
andersbandt <andersbandt@yahoo.com>
2019-10-25 12:42:31 -0500

    Version 2
    Adjusted settings for gpio control as new Raspberry Pi setup does not have WiringPi installed the same way as before

--- relay-control.php
+++ relay-control.php
@@ -6,14 +6,16 @@
          <body>
          LED Control:
          <form method="get" action="relay-control.php">
-                 <input type="submit" value="ON" name="on">
-                 <input type="submit" value="OFF" name="off">
+                 <input type="submit" value="Outtake On" name="outtake_on">
+                 <input type="submit" value="Outtake Off" name="outtake_off">
+                 <input type="submit" value="Intake On" name="intake_on">
+                 <input type="submit" value="Intake Off" name="intake_off">
                  <input type="text" name="hour">
                  <input type="text" name="minute">
                  <input type="submit" value="OFF" name="submit">
          </form>
          <?php
-         $setmode17 = shell_exec("/usr/local/bin/gpio -g mode 17 out");
+         $setmode17 = shell_exec("");
          if(isset($_GET['final'])){
                  $hour = $_GET['hour'];
                  $minute = $_GET['minute']; 
@@ -22,13 +24,21 @@
                  file_put_contents('/tmp/crontab.txt', $output.$command.PHP_EOL);
                  echo exec('crontab /tmp/crontab.txt');
          }
-         if(isset($_GET['on'])){
-                 $gpio_on = shell_exec("/usr/local/bin/gpio -g write 17 1");
-                echo "LED is on";
+         if(isset($_GET['intake_on'])){
+                 $gpio_on = shell_exec("./Code/grow/intake_on.sh");
+                echo "Intake Fan is On";
          }
-         if(isset($_GET['off'])){
-                 $gpio_off = shell_exec("/usr/local/bin/gpio -g write 17 0");
-                 echo "LED is off";
+         if(isset($_GET['intake_off'])){
+                 $gpio_off = shell_exec(".Code/grow/intake_off.sh");
+                 echo "Intake Fan is Off";
+         }
+         if(isset($_GET['outtake_on'])){
+                 $gpio_on = shell_exec("./Code/grow/outtake_on.sh");
+                echo "Intake Fan is On";
+         }
+         if(isset($_GET['outtake_off'])){
+                 $gpio_off = shell_exec(".Code/grow/outtake_off.sh");
+                 echo "Intake Fan is Off";
          }
          ?>
          </body>
